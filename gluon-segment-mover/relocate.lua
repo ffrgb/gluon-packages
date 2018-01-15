55575694e5a9a8b4e63008fd74f8c1a59187b27f
Bastian <mephisto@mephis.to>
2018-01-16 00:05:57 +0100

    some work on relocator

--- gluon-segment-mover/relocate.lua
+++ gluon-segment-mover/relocate.lua
@@ -1,32 +1,29 @@
 #!/usr/bin/lua
-
 --[[
-
 Segment Mover
 
-UCI Key:
-- currentsite.current.override='false' Preserves Segment, even if Gateway requests relocation
+Cronjob that asks a Director Server, if the Node should move to another Segment.
+This is mainly for Migration purposes when Domainsplits are done and can be
+removed from the firmware when everything is done.
 
+UCI Keys:
+- currentsite.current.override='false': Preserves Segment, even if Gateway requests relocation
 --]]
 
+local directorurl="http://fdef:f00f:1337:cafe::11/director/move.php?id="
 local o=require('luci.model.uci').cursor()
 local o=o:get("currentsite","current","override")
-
 if (o=="true") then
         io.write("Override")
         do return end
 end
-
 local o=require('luci.model.uci').cursor()
 local o=o:get("currentsite","current","name")
-io.write(o .. "\n")
-
 local nodeid = require('gluon.util').node_id()
-local newseg = io.popen("wget -q -O - http://fdef:f00f:1337:cafe::11/director/move.php?id=" .. nodeid):read('*a')
+local newseg = io.popen("wget -q -O - " .. directorurl .. nodeid):read('*a')
 io.write("Current Segment: " .. o .. "\nNodeID: " .. nodeid .. "\nRequested Segment: " .. newseg .."\n")
-
 if (o==newseg) then
-        io.write("No Change\n");
+        io.write("No Change\n")
 else
-        io.write("Change\n");
+        io.write("Change\n")
 end
