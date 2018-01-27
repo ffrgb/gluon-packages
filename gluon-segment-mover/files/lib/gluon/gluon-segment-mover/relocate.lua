19010fc82130253caec04783e39da2e8f6ed6431
Bastian <mephisto@mephis.to>
2018-01-27 19:42:21 +0100

    Adjusted for latest Gluon Master w/ merged multidomain features

--- gluon-segment-mover/files/lib/gluon/gluon-segment-mover/relocate.lua
+++ gluon-segment-mover/files/lib/gluon/gluon-segment-mover/relocate.lua
@@ -1,29 +1,21 @@
 #!/usr/bin/lua
-math.randomseed(os.time())
-local number = math.random(240)
-os.execute('sleep ' .. number)
+--math.randomseed(os.time())
+--local number = math.random(240)
+--os.execute('sleep ' .. number)
 function file_exists(name)
   local f=io.open(name,"r")
   if f~=nil then io.close(f) return true else return false end
 end
 local uci=require('simple-uci').cursor()
---local directorurl=uci:get('segmentmover','url')
--- local directorurl='http://test.ffrgb/'
---if (directorurl == nil) then
---  io.write('No URL given. Exiting..\n')
---  do return end
---end
 local directorurl='http://director.services.ffrgb/move.php?nodeid='
 local o=uci:get('segmentmover','override')
 if (o=='true') then
   io.write("Override. Exiting..")
   do return end
 end
-local o=uci:get("gluon","system","domain_code")
+local o=uci:get("gluon","core","domain")
 local nodeid = require('gluon.util').node_id()
 local newseg = io.popen("wget -q -O - " .. directorurl .. nodeid):read('*a')
--- for testing only
--- local newseg = 'ffrgb_cty'
 io.write('Current Segment: ' .. o .. '\nNodeID: ' .. nodeid .. '\nRequested Segment: ' .. newseg ..'\n')
 if (o==newseg or newseg == "") then
   io.write("Do nothing..\n")
@@ -34,7 +26,7 @@ else
     uci:save('gluon')
     uci:commit('gluon')
     io.write("Change\n")
-    os.execute('/lib/gluon/domain_changed.sh')
+    os.execute('/usr/bin/gluon-reconfigure')
     io.popen("reboot")
   else
     io.write('Invalid Segment requested. I don\'t have ' .. newseg .. '.conf')
