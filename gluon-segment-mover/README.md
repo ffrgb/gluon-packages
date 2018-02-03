dd5de04abe643a4fdeec2d1d5bc749e72904dc4e
Bastian <mephisto@mephis.to>
2018-02-03 14:35:43 +0100

    fixed delay, changed ignore uci value, implemented vxlan/legacy switcher

--- gluon-segment-mover/README.md
+++ gluon-segment-mover/README.md
@@ -16,9 +16,30 @@ This is mainly for Migration purposes when Domainsplits are done and can be
 removed from the firmware when everything is done.
 
 UCI Keys:
-- currentsite.current.override='false': Preserves Segment, even if Gateway requests relocation
-- gluon.system.domaincode: Node Domain Code
+- gluon.core.ignorerelocate='1': Ignore Domain Change requests
+- gluon.core.domain: Node Domain Code
 
 Status:
 
 Currently edit the relocator.lua file and let it point to a Script on a webserver, that simply returns the desired domain_code as defined in the Gluon Config for that nodeid. For the Moment I don't include a Backend for that. This will happen later perhaps.
+
+A very Simple Script would look like:
+
+```
+<?php
+$relocator = array (
+        '0019995f8a79' => 'ffrgb_uml',
+        'a42bb0d87a00' => 'ffrgb_cty', #test
+        '60e3279fc038' => 'ffrgb_uml', #LQ_FahrschuleHofer_01
+        'a42bb0c920e0' => 'ffrgb_uml', #LQ_FahrschuleHofer_02 VPN
+        '60e327ceea24' => 'ffrgb_uml'
+);
+if (!isset($relocator[$_GET['nodeid']])) {
+        echo "noop";
+} else {
+        echo $relocator[$_GET['nodeid']];
+}
+?>
+
+```
+.. but you are free to get this Database driven or whatever..
--- gluon-segment-mover/files/lib/gluon/gluon-segment-mover/relocate.lua
+++ gluon-segment-mover/files/lib/gluon/gluon-segment-mover/relocate.lua
@@ -1,35 +1,50 @@
 #!/usr/bin/lua
---math.randomseed(os.time())
---local number = math.random(240)
---os.execute('sleep ' .. number)
+local devurandom = io.open("/dev/urandom","rb")
+local b1,b2 = devurandom:read(2):byte(1,2)
+seed = b1 + (256 * b2)
+math.randomseed(seed)
+local delay = math.random(60)
 function file_exists(name)
   local f=io.open(name,"r")
   if f~=nil then io.close(f) return true else return false end
 end
 local uci=require('simple-uci').cursor()
-local o=uci:get('segmentmover','override')
-if (o=='true') then
-  io.write("Override. Exiting..")
+local o=uci:get('gluon','core','ignorerelocate')
+if (o=='1') then
+  io.write("Ignoring Domain Change request.. Exiting.\n")
   do return end
 end
+io.write('Sleeping for ' .. delay .. ' Seconds.\n')
+os.execute('sleep ' .. delay)
 local currentdomain=uci:get("gluon","core","domain")
 local nodeid = require('gluon.util').node_id()
 local directorurl='http://director.services.ffrgb/move.php?nodeid=' .. nodeid .. '&currentdomain=' .. currentdomain
 local newseg = io.popen("wget -q -O - '" .. directorurl .. "'"):read('*a')
-io.write('Current Segment: ' .. currentdomain .. '\nNodeID: ' .. nodeid .. '\nRequested Segment: ' .. newseg ..'\n')
+io.write('Current Domain: ' .. currentdomain .. '\nNodeID: ' .. nodeid .. '\nRequested Domain: ' .. newseg ..'\n')
 if (currentdomain==newseg or newseg == "" or newseg == "noop") then
   io.write("Do nothing..\n")
 else
   if (file_exists('/lib/gluon/domains/' .. newseg .. '.json') == true) then
-    os.execute('logger -s -t "gluon-segment-mover" -p 5 "Segment Change requested. Moving to "' .. newseg)
+    os.execute('logger -s -t "gluon-segment-mover" -p 5 "Domain Change requested. Moving to "' .. newseg)
     uci:set('gluon','core','domain',newseg)
     uci:save('gluon')
     uci:commit('gluon')
-    io.write("Change\n")
+    -- FFRGB Specific: Default VXLan on Domains ~= FFRGB
+    if (newseg ~= 'ffrgb') then
+      uci:set ('network','mesh_wan','legacy',0)
+      uci:set ('network','mesh_lan','legacy',0)
+      uci:save ('network')
+      uci:commit ('network')
+    else
+      uci:set ('network','mesh_wan','legacy',1)
+      uci:set ('network','mesh_lan','legacy',1)
+      uci:save ('network')
+      uci:commit ('network')
+    end
     os.execute('/usr/bin/gluon-reconfigure')
     io.popen("reboot")
   else
-    io.write('Invalid Segment requested. I don\'t have ' .. newseg .. '.conf')
+    io.write('Invalid Domain requested. I don\'t have ' .. newseg .. '.conf')
     do return end
   end
 end
